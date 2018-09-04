c3f7a45ace56ebb242436d0562a0452d55a9b0b4
ewertonhm <30476434+ewertonhm@users.noreply.github.com>
2018-09-04 10:14:08 -0300

    Cadastro de usuários

--- cadastrar.php
+++ cadastrar.php
@@ -5,40 +5,47 @@
 	// Sessão
 	session_start();
 
+	// Include
+	include "functions.php";
+
 	// Verificar se o botão ja foi clicado
-	if(isset($_POST['btn-entrar'])):
+	if(isset($_POST['btn-cadastrar'])):
 		//array para salvar as mensagens de erro
 		$erros = array();
         // passa os dados vindos do $_POST em suas respectivas variaveis
-        $nome = mysqli_escape_string($link, $_POST['nome']);
-        $email = mysqli_escape_string($link, $_POST['email']);
-        $cpf = mysqli_escape_string($link, $_POST['cpf']);
-        $bdate = mysqli_escape_string($link, $_POST['bdate']);
-        $celular = mysqli_escape_string($link, $_POST['celular']);
-        $endereco = mysqli_escape_string($link, $_POST['endereco']);
-        $login = mysqli_escape_string($link, $_POST['login']);
-        $senha = mysqli_escape_string($link, $_POST['senha']);
+        $nome = cleanstring($_POST['nome']);
+        $email = cleanstring($_POST['email']);
+        $cpf = cleanstring($_POST['cpf']);
+        $bdate = cleanstring($_POST['bdate']);
+        $celular = cleanstring($_POST['celular']);
+        $endereco = cleanstring($_POST['endereco']);
+        $login = cleanstring($_POST['login']);
+        $senha = cleanstring($_POST['senha']);
 
         //echo $nome.$email.$cpf.$bdate.$celular.$login.$senha;
         // verifica se as variaveis (campos) estão vazias
         //if(empty($login) or empty($senha) or empty($email) or empty($cpf) or empty($celular) or empty($nome)):
-        if(empty($login) or empty($senha) or empty($nome)):    
+        if(empty($login) or empty($senha) or empty($nome) or empty($email)):    
 			$erros[] = "<li> Todos os campos devem ser preenchidos. </li>";
         else:
             // verificar se o usuario ja existe (melhorar) ******************************
             //$query = "SELECT * FROM usuarios WHERE login = '$login' OR email = '$email' OR cpf = '$cpf'";
             $query = "SELECT * FROM usuarios WHERE login = '$login'";
-			$consulta_usuario = mysqli_query($link, $query);
-			$resultado = mysqli_fetch_assoc($consulta_usuario);
-			if(isset($resultado)):
-				$erros[] = "<li> Email ou senha ou CPF ja cadastrado </li>";
+			$consulta_usuario = pg_query($dbconn, $query);
+			$resultado = pg_fetch_all($consulta_usuario);
+			if($resultado['0']['login'] == $login):
+				$erros[] = "<li> Login já cadastrado </li>";
+			elseif($resultado['0']['email'] == $email):
+				$erros[] = "<li> Email já cadastrado </li>";
+			elseif($resultado['0']['cpf'] == $cpf):
+				$erros[] = "<li> CPF já cadastrado </li>";
             else:
                 // insere os dados no banco
 			    $senha = md5($senha);
-			    $query = "  INSERT INTO `usuarios` (`id`, `nome`, `cpf`, `email`, `bdate`, `celular`, `endereco`, `login`, `senha`) 
-                            VALUES (NULL, '$nome', '$cpf', '$email', '$bdate', '$celular', '$endereco', '$login','$senha');";
-			    if(mysqli_query($link, $query)):
-			    //$resultado = mysqli_fetch_assoc($cadastrar_usuario);
+			    $query = "INSERT INTO usuarios (nome, email, cpf, bdate, celular, endereco, login, senha) 
+                            VALUES ('$nome', '$cpf', '$email', '$bdate', '$celular', '$endereco', '$login','$senha');";
+			    if(pg_query($dbconn, $query)):
+			    //$resultado = pg_fetch_all($cadastrar_usuario);
                 $erros[] = "<li> Cadastro realizado com sucesso</li>";
                 else:
                     echo "deu ruim";
@@ -52,6 +59,8 @@
 	<head>
 		<title>Cadastro</title>
 		<meta charset="utf-8">
+		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
 	</head>
 	<body>
 		<h1> Cadastro </h1>
@@ -73,7 +82,7 @@
             Endereço: <input type="text" name="endereco"><br>
             Login: <input type="text" name="login"><br>
 			Senha: <input type="password" name="senha"><br>
-			<button type="submit" name="btn-entrar">Cadastrar</button>
+			<button type="submit" name="btn-cadastrar">Cadastrar</button>
 		</form>
 	</body>	
 </html>	
--- db.sql
+++ db.sql
@@ -2,6 +2,7 @@ CREATE DATABASE database;
 CREATE TABLE usuarios(
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
     cpf VARCHAR(15) NOT NULL,
     bdate DATE,
     celular VARCHAR(15),
@@ -9,5 +10,5 @@ CREATE TABLE usuarios(
     login VARCHAR(255) NOT NULL,
     senha VARCHAR(32) NOT NULL
 );
-INSERT INTO usuarios (nome, cpf, bdate, celular, endereco, login, senha) 
-VALUES ('Administrador', '000.000.000-00', '01-01-1900', '00 00000 0000', 'Rua xxxxxxxxxxxx, Nº xxxxxxxxxx', 'admin', MD5('123456'));
+INSERT INTO usuarios (nome, email, cpf, bdate, celular, endereco, login, senha) 
+VALUES ('Administrador', 'administrador@local.com', '000.000.000-00', '01-01-1900', '00 00000 0000', 'Rua xxxxxxxxxxxx, Nº xxxxxxxxxx', 'admin', MD5('123456'));
