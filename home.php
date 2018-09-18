ad1066ba3898e386523edbac84467d2295f63486
ewertonhm <ewerton.4vips@hotmail.com>
2018-09-18 20:33:03 -0300

    footer

--- home.php
+++ home.php
@@ -9,32 +9,64 @@
     include "includes/navbar.php";
 ?>
 <!-- <body> -->
-        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item">
-                <img class="img-fluid d-block" src="img/slide-01.jpg?auto=yes&bg=777&fg=555&text=First slide" alt="First slide" >
-            </div>
-            <div class="carousel-item">
-                <img class="img-fluid d-block" src="img/slide-02.jpg?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
-            </div>
-            <div class="carousel-item">
-                <img class="img-fluid d-block" src="img/slide-03.jpg?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide">
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>    
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="img/slide-01.jpg" alt="First slide">
     </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/slide-02.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/slide-03.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<div class="container">
+<p>
+Lorem ipsum ut primis dolor tortor viverra per laoreet proin, etiam laoreet suscipit curabitur maecenas id ut dapibus viverra, et quisque ligula at platea taciti ligula commodo. 
+fusce fames sodales himenaeos pulvinar ut himenaeos id nullam, curae euismod cursus curabitur posuere blandit quisque, vehicula ultrices conubia auctor orci varius per. 
+sodales nisi justo pretium orci eleifend hac nibh, dolor ultricies vitae ultrices proin mi platea duis, elit faucibus praesent himenaeos potenti nullam. 
+feugiat ultricies iaculis metus maecaenas pellentesque id ligula in sed, non erat proin vestibulum habitant urna egestas condimentum non, hendrerit ac hac netus sem phasellus sollicitudin tortor. 
+</p>
+<p>
+Elit eros nibh nulla fringilla tristique neque cursus, ultrices odio ipsum viverra primis sem cubilia leo, nostra duis erat himenaeos nulla rutrum. 
+mauris facilisis non ornare primis class dapibus enim nullam gravida purus ligula sed, primis eget neque sed tellus nulla nam vivamus suscipit mi aenean, ut ligula diam augue cubilia litora etiam risus vivamus duis consequat. 
+fames mollis lacinia porta a aliquam at nunc, nec a sociosqu ut nunc leo gravida leo, tincidunt pellentesque turpis donec bibendum cubilia. 
+urna massa molestie curae imperdiet tincidunt sagittis magna pellentesque nisi, faucibus ultricies duis porta rhoncus tristique consectetur dui placerat vivamus, lacinia sagittis nec nostra pulvinar fermentum eros interdum. 
+</p>
+<p>
+Nisl dui aptent est neque turpis auctor taciti, dui pulvinar donec dapibus sit hendrerit eros senectus, phasellus porttitor imperdiet cubilia lobortis feugiat. 
+primis posuere tellus per posuere condimentum eget dui laoreet etiam, nostra dolor adipiscing porttitor quisque dui cras felis, egestas lacus pharetra suspendisse primis nullam varius leo. 
+donec duis quis nisi volutpat purus interdum accumsan augue sed odio, urna odio gravida tincidunt odio nulla eleifend iaculis hendrerit interdum, vitae donec quisque vitae lobortis primis condimentum rutrum nullam. 
+nunc dui tristique ante tellus ligula eget primis ultrices suscipit odio diam arcu etiam, lacus fusce tortor nisl vestibulum vitae arcu at arcu eleifend venenatis. 
+</p>
+<p>
+Id sodales sem proin ultrices ultricies ac mauris erat tempor feugiat, eu nec dui conubia litora aenean dictumst ante egestas, congue iaculis curabitur consequat enim quisque elit tristique sociosqu. 
+sit at habitant elit praesent sociosqu sodales cursus varius, quam cubilia malesuada turpis ut orci aliquet aenean, primis volutpat amet cubilia tincidunt nam vulputate. 
+id blandit nunc blandit vestibulum donec ultrices habitant, condimentum auctor consequat dictum urna purus ultricies, sem convallis eu ut a ultrices. 
+fringilla est quisque velit aenean nulla class scelerisque, ipsum pellentesque consequat vitae gravida id, elementum laoreet dui a tristique feugiat. 
+</p>
+<p>
+Dui himenaeos nullam ut ullamcorper hendrerit sodales adipiscing hendrerit curabitur, quis accumsan enim fusce et nam quisque bibendum, lacus dapibus aptent quis curae molestie sapien pellentesque. 
+quisque senectus orci lobortis vehicula sit, id rhoncus id convallis eget hendrerit, turpis proin sem viverra. 
+vehicula cubilia neque ad curabitur hac mi nam molestie porttitor posuere, orci eleifend suspendisse potenti turpis aptent in quisque velit senectus sagittis, interdum aenean nullam maecenas id luctus auctor eros dictum. 
+hac nunc odio integer pretium porttitor phasellus viverra curabitur, ultrices risus diam elementum suspendisse egestas quis augue, senectus rutrum platea luctus quis vestibulum curabitur. 
+</p>
+</div>
 <!-- </body> -->
 <?php include "includes/bottom.php";?>
 <!-- </html> -->
--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,29 @@
-<?php
-    echo "
-        <footer>
-        </footer>
-    "
-?>
+
+   <!-- Footer -->
+    <footer class='footer modal-footer '>
+    
+        <!-- Footer Elements -->
+        <div class='container text-center'>
+    
+          <!-- Call to action -->
+          <ul class='list-unstyled'>
+            <li>
+              <h5>Registre-se gratuitamente</h5>
+            </li>
+            <li>
+              <button class='btn btn-small btn-primary btn-dark' href='cadastrar.php'>Registrar-se!</a>
+            </li>
+          </ul>
+          <!-- Call to action -->
+    
+        
+        <!-- Footer Elements -->
+    
+        <!-- Copyright -->
+        <div class='footer-copyright text-center py-3'>© 2018 Copyright:
+          <a href='#'> PuroSom.Local</a>
+        </div>
+        <!-- Copyright -->
+        </div>
+      </footer>
+      <!-- Footer -->
