1bd6a365fa0c34df4bdf41f7c7b7bce4224af461
ewertonhm <30476434+ewertonhm@users.noreply.github.com>
2018-09-18 08:41:12 -0300

    Revert "Local Bootstrap"
    This reverts commit 4b445958c6ebe2108e9cbf3b17d74530f4fdb64b.

--- login.php
+++ login.php
@@ -57,21 +57,21 @@
 	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     	<meta name="description" content="">
     	<meta name="author" content="Ewerton H Marschalk">
-    	<link rel="icon" href="images/favicon.ico">
+    	<link rel="icon" href="https://getbootstrap.com/favicon.ico">
 
     	<!-- Custom styles for this template -->
     	<link href="css/login.css" rel="stylesheet">
-   		<!-- Bootstrap CSS -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
+   		<!-- Bootstrapcdn CSS -->
+		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
 		
 		<!-- Bootstrapcdn JS -->
-		<script src="css/bootstrap.min.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
 
 	</head>
 	<body class="text-center">
 		<form class="form-signin" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
 			<div class="text-center mb-4">
-				<img class="mb-4" src="images/bootstrap-solid.svg" alt="" width="72" height="72">
+				<img class="mb-4" src="https://getbootstrap.com/docs/4.1/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
 				<h1 class="h3 mb-3 font-weight-normal">Entrar na sua conta</h1>
 				<?php
 					// se existir erros, exibe
