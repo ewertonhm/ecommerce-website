58f67108e64f2f53005bbfdc86230ad6fe3575f3
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2018-09-28 22:03:33 -0300

    unused files

--- teste.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'classes/email.php';
-
-$mail = new email();
-
-$mail->setAssunto('teste de email php');
-$mail->setDestinatario('ewertonhm@outlook.com');
-$mail->setMensagem('teste teste teste');
-$mail->enviar();
